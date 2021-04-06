6f356f45d882873379a6e7c9347be8d6925f255d
wanhb <27332841@qq.com>
2021-04-06 15:58:24 +0800

    增加model 1. 增加model基类sql.class.php

---
+++ application/controllers/Controller.class.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * 控制器基类
+ * */
+
+class Controller
+{
+    protected $_controler;
+    protected $_action;
+    protected $_view;
+
+    // 构造函数,初始化属性,并实例化对应模型
+    function __construct($controler, $action)
+    {
+        $this->_controler = $controler;
+        $this->_action = $action;
+
+//        $this->_view = new View
+    }
+
+    // 分配变量
+
+    /**
+     * @return mixed
+     */
+    public function assign($name, $value)
+    {
+        $this->_view->assign($name, $value);
+    }
+
+    // 渲染视图
+    function __destruct()
+    {
+        $this->_view->render();
+    }
+
+}
