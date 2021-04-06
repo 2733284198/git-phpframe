defcefe8eead3d4cc71100f82915674a62b4de05
wanhb <27332841@qq.com>
2021-04-06 14:23:58 +0800

    php框架: 1. 类自动加载

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// 加载工具类
+require_once "tools.php";
+printLine("index.php");
+
+// 应用程序目录
+define('APP_PATH', __DIR__.'/');
+
+// 开启调试模式
+define('APP_DEBUG', true);
+
+// 网站根url
+define('APP_URL', 'http://localhost/phpframe/');
--- tools.php
+++ tools.php
@@ -1,7 +1,11 @@
 <?php
 
 if (!function_exists(printLine)){
-    function printLine(){
-        echo '<br><hr>--debug--<br><hr>';
+    function printLine($title = 'debug'){
+        if (strcmp($title, 'debug')){
+            echo "<br><hr>--{$title}--<br><hr>";
+        }else{
+            echo "<br><hr>--debug--<br><hr>";
+        }
     }
 }
